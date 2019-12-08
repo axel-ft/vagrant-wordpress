8eda0f88b8865010648ace335c2822d77bb8c673
Axel Floquet-Trillot <Axel@casa.velizy>
2019-12-08 19:08:11 +0100

    Initial commit exo 2

--- common/sethosts.sh
+++ common/sethosts.sh
@@ -11,8 +11,9 @@
 sed -i -e "s/.*127.0.0.1.*localhost.*/127.0.0.1\\tlocalhost\\t${1}/" /etc/hosts
 
 # Add one line for each host if it does not already exists (in case of reprovisioning)
-grep -qxF "${mariadb_ip}$(printf '\t')${mariadb_hostname}" /etc/hosts || echo -e "${mariadb_ip}\\t${mariadb_hostname}"        >> /etc/hosts
-grep -qxF "${nginx_ip}$(printf '\t')${nginx_hostname}"     /etc/hosts || echo -e "${nginx_ip}$(printf '\t')${nginx_hostname}" >> /etc/hosts
+grep -qxF "${squid_ip}$(printf '\t')${squid_hostname}"     /etc/hosts || echo -e "${squid_ip}\\t${squid_hostname}"     >> /etc/hosts
+grep -qxF "${mariadb_ip}$(printf '\t')${mariadb_hostname}" /etc/hosts || echo -e "${mariadb_ip}\\t${mariadb_hostname}" >> /etc/hosts
+grep -qxF "${nginx_ip}$(printf '\t')${nginx_hostname}"     /etc/hosts || echo -e "${nginx_ip}\\t${nginx_hostname}"     >> /etc/hosts
 
 # Display progress bar if command is in path and current progress in provisioning given
 which progressbar 2>&1>/dev/null && [ ${2} ] && progressbar ${2}
