f8ba425a7ed5e003004a36f05203b9657faf8678
mpoulier <matthieu.poulier@ynov.com>
2020-01-16 12:15:06 +0100

    Update files

--- centreon/install.sh
+++ centreon/install.sh
@@ -93,12 +93,16 @@ NAME="Centreon Engine"
 echo -e "\n##################################################################\n"
 echo -e "\n##########                                              ##########\n"
 echo -e "\n##########                   ${NAME}                    ##########\n"
-echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                                              ##########\n"²²
 echo -e "\n##################################################################\n"
 
     sudo groupadd -g 6001 centreon-engine
     sudo useradd -u 6001 -g centreon-engine -m -r -d /var/lib/centreon-engine \
         -c "Centreon-engine Admin" -s /bin/bash centreon-engine
+
+    wget -q "${CENTREON_ENGINE}"
+    tar -xzf "${CENTREON_ENGINE_TGZ}" 
+    pushd "${CENTREON_ENGINE_DIR}" > /dev/null
 
     wget "${CENTREON_ENGINE}"
     tar -xzf "${CENTREON_ENGINE_TGZ}" 
@@ -144,76 +148,117 @@ wget "${CENTREON_ENGINE_PLUGIN}"
 tar -xzf "${CENTREON_ENGINE_PLUGIN_TGZ}"
 cd nagios-plugins-2.2.1
 
-cd
-wget --no-check-certificate https://www.monitoring-plugins.org/download/monitoring-plugins-2.2.tar.gz
+        cmake  \
+            -DWITH_CENTREON_CLIB_INCLUDE_DIR=/usr/include  \
+            -DWITH_CENTREON_CLIB_LIBRARY_DIR=/usr/lib  \
+            -DWITH_PREFIX=/usr  \
+            -DWITH_PREFIX_BIN=/usr/sbin  \
+            -DWITH_PREFIX_CONF=/etc/centreon-engine  \
+            -DWITH_USER=centreon-engine  \
+            -DWITH_GROUP=centreon-engine  \
+            -DWITH_LOGROTATE_SCRIPT=1 \
+            -DWITH_VAR_DIR=/var/log/centreon-engine  \
+            -DWITH_RW_DIR=/var/lib/centreon-engine/rw  \
+            -DWITH_STARTUP_SCRIPT=systemd  \
+            -DWITH_STARTUP_DIR=/lib/systemd/system  \
+            -DWITH_PKGCONFIG_SCRIPT=1 \
+            -DWITH_PKGCONFIG_DIR=/usr/lib/pkgconfig  \
+            -DWITH_TESTING=0  .
 
-tar -xzf monitoring-plugins-2.2.tar.gz
-cd monitoring-plugins-2.2
+        make
+        sudo make install
+    popd > /dev/null
+    sudo systemctl enable centengine.service
+    sudo systemctl daemon-reload
 
-./configure --with-nagios-user=centreon-engine --with-nagios-group=centreon-engine --prefix=/usr/lib/nagios/plugins --libexecdir=/usr/lib/nagios/plugins --enable-perl-modules --with-openssl=/usr/bin/openssl
-make 
-sudo make install
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
 
-cd
-wget http://files.download.centreon.com/public/centreon-plugins/centreon-plugins-20191016.tar.gz
-tar xzf centreon-plugins-20191016.tar.gz 
-cd centreon-plugins-20191016
+NAME="Centreon Engine Plugins"
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                   ${NAME}                    ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
 
-cd
-wget http://files.download.centreon.com/public/centreon-plugins/centreon-plugins-20190704.tar.gz
-tar xzf centreon-plugins-20190704.tar.gz 
-cd centreon-plugins-20190704
+    wget -q "${NAGIOS_PLUGIN}"
+    tar -xzf "${NAGIOS_PLUGIN_TGZ}"
 
-sudo cp centreon_centreon_database.pl /usr/lib/centreon/plugins/
-sudo cp centreon_mysql.pl /usr/lib/centreon/plugins/
+    wget -q --no-check-certificate "${MONITORING_PLUGINS}"
 
-chmod +x *
-sudo mkdir -p /usr/lib/centreon/plugins
-sudo mv * /usr/lib/centreon/plugins/
+    tar -xzf "${MONITORING_PLUGINS_TGZ}"
+    pushd "${MONITORING_PLUGINS_DIR}" > /dev/null
+    ./configure --with-nagios-user=centreon-engine --with-nagios-group=centreon-engine --prefix=/usr/lib/nagios/plugins --libexecdir=/usr/lib/nagios/plugins --enable-perl-modules --with-openssl=/usr/bin/openssl
+    make 
+    sudo make install
+    popd > /dev/null
+
+    wget -q "${CENTREON_PLUGINS}"
+    tar -xzf "${CENTREON_PLUGINS_TGZ}" 
+    pushd "${CENTREON_PLUGINS_DIR}" > /dev/null
+        chmod +x *
+        sudo mkdir -p /usr/lib/centreon/plugins
+        sudo mv * /usr/lib/centreon/plugins/
+    popd > /dev/null
+
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+NAME="Centreon Broker"
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                   ${NAME}                    ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+    sudo groupadd -g 6002 centreon-broker
+    sudo useradd -u 6002 -g centreon-broker -m -r -d /var/lib/centreon-broker -c "Centreon-broker Admin" -s /bin/bash centreon-broker
+
+    sudo usermod -aG centreon-broker centreon-engine
 
 
+    wget -q "${CENTREON_BROKER}"
+    tar -xzf "${CENTREON_BROKER_TGZ}"
+    pushd "${CENTREON_BROKER_DIR}" > /dev/null
+    cmake \
+         -DWITH_DAEMONS='central-broker;central-rrd' \
+         -DWITH_GROUP=centreon-broker \
+         -DWITH_PREFIX=/usr  \
+         -DWITH_PREFIX_BIN=/usr/sbin  \
+         -DWITH_PREFIX_CONF=/etc/centreon-broker  \
+         -DWITH_PREFIX_LIB=/usr/lib/centreon-broker \
+         -DWITH_PREFIX_VAR=/var/lib/centreon-broker \
+         -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker \
+         -DWITH_STARTUP_SCRIPT=systemd  \
+         -DWITH_STARTUP_DIR=/lib/systemd/system  \
+         -DWITH_TESTING=0 \
+         -DWITH_USER=centreon-broker .
 
-################################################################################
-##########                                                            ##########
-##########                      Centreon Broker                       ##########
-##########                                                            ##########
-################################################################################
-
-sudo groupadd -g 6002 centreon-broker
-sudo useradd -u 6002 -g centreon-broker -m -r -d /var/lib/centreon-broker -c "Centreon-broker Admin" -s /bin/bash centreon-broker
-
-sudo usermod -aG centreon-broker centreon-engine
-
-cd
-wget http://files.download.centreon.com/public/centreon-broker/centreon-broker-19.10.0.tar.gz
-tar xzf centreon-broker-19.10.0.tar.gz
-cd centreon-broker-19.10.0
-
-cmake \
-     -DWITH_DAEMONS='central-broker;central-rrd' \
-     -DWITH_GROUP=centreon-broker \
-     -DWITH_PREFIX=/usr  \
-     -DWITH_PREFIX_BIN=/usr/sbin  \
-     -DWITH_PREFIX_CONF=/etc/centreon-broker  \
-     -DWITH_PREFIX_LIB=/usr/lib/centreon-broker \
-     -DWITH_PREFIX_VAR=/var/lib/centreon-broker \
-     -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker \
-     -DWITH_STARTUP_SCRIPT=systemd  \
-     -DWITH_STARTUP_DIR=/lib/systemd/system  \
-     -DWITH_TESTING=0 \
-     -DWITH_USER=centreon-broker .
-
-make
-sudo make install
+    make
+    sudo make install
+    popd > /dev/null
 
 sudo systemctl enable cbd.service
 sudo systemctl daemon-reload
 
-################################################################################
-##########                                                            ##########
-##########                       SNMP Protocole                       ##########
-##########                                                            ##########
-################################################################################
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+NAME="SNMP Protocole"
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                   ${NAME}                    ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
 
 cat << SNMPD > /etc/snmp/snmpd.conf
 agentAddress udp:localhost:161
@@ -237,16 +282,81 @@ TRAPDOPTS='-On -Lsdf /var/log/snmptrapd.log -p /run/snmptrapd.pid'
 .....
 TRAPD
 
-################################################################################
-##########                                                            ##########
-##########                         MIBS                               ##########
-##########                                                            ##########
-################################################################################
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+NAME="MIBS"
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                   ${NAME}                    ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+    export MIBDIRS=/usr/shaqre/MIBS
+    export MIBS=ALL
+
+cat << SNMP > /etc/snmp/snmp.conf
+#mibs ALL 
+SNMP
+
+    sudo systemctl restart snmpd
+    sudo systemctl restart snmptrapd
+
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+NAME="Centreon UI"
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                   ${NAME}                    ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
+
+    sudo groupadd -g 6000 centreon
+    sudo useradd -u 6000 -g centreon -m -r -d /var/lib/centreon -c "Centreon Admin" -s /bin/bash centreon
+
+    sudo usermod -aG centreon centreon-broker
+
+    sudo apt install rrdtool ntp php7.3-sqlite3 php-pear sudo tofrodos bsd-mailx lsb-release mariadb-server libconfig-inifiles-perl libcrypt-des-perl libdigest-hmac-perl libdigest-sha-perl libgd-perl php7.3-ldap php7.3-snmp -y
+
+    sudo a2enmod proxy_fcgi setenvif proxy rewrite
+    sudo a2enconf php7.3-fpm
+    sudo a2dismod php7.3
+    sudo systemctl restart apache2 php7.3-fpm
+
+# Nouveauté avec la version 19.10.x, il faut obligatoirement configurer le paramètre date.timezone pour le php-fpm. Pour cela, éditez le fichier de configuration pour apache. Attention, bien respectez la casse et ne pas mettre d'espace.
+# sudo vi /etc/php/7.3/fpm/php.ini
+# Saisissez la valeur adaptée à votre configuration.
+# [Date]
+# ; Defines the default timezone used by the date functions
+# ; http://php.net/date.timezone
+# date.timezone = Europe/Paris
 
 
+    wget -q "${CENTREON_UI}"
+    tar -xzf "${CENTREON_UI_TGZ}"
+    pushd "${CENTREON_UI_DIR}"
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+        composer install --no-dev --optimize-autoloader
+    popd > /dev/null
 
-################################################################################
-##########                                                            ##########
-##########                         Centreon UI                        ##########
-##########                                                            ##########
-################################################################################
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+
+    sudo npm install
+    sudo npm run build
+
+# sudo bash ./install.sh -i - test 
+
+echo -e "\n##################################################################\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##########                    Done                      ##########\n"
+echo -e "\n##########                                              ##########\n"
+echo -e "\n##################################################################\n"
